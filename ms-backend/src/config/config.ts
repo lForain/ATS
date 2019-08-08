13a2a2437543525da87ba09a86f6fe18f318f9a7
Lucas dos Santos Forain <Lucas.Forain@ibm.com>
2019-08-08 09:40:10 -0300

    34 config mongo (#36)
    * set config.ts and server.ts to our case, with the required imports, install dependencies and created basic routes.
    
    * start script path fix.
    
    * change useMongo on config.ts to true
    
    * create .env file, fix connection string
    
    * retire .env file from gitignore
    
    * changed app from require to import method
    
    * include MONGO_DSN to .env, change serve.ts to always try connection to mongo, change config to always connect to MONGO_DSN string
    
    * create env-example and add .env to gitignore
    
    * Delete. env
    
    * add type to connection string
    
    * fix env-example to got only the var name, so every one use their own connection string

--- ms-backend/src/config/config.ts
+++ ms-backend/src/config/config.ts
@@ -1,3 +1,12 @@
+import { sync } from "glob";
+import { union } from "lodash";
+
 export default class Config {
-    public static port: number = 3000;
+  public static port: number = 3000;
+  public static routes: string = "./dist/routes/**/*.js";
+  public static models: string = "./dist/models/**/*.js";
+  public static mongodb: string = process.env.MONGODB_DSN;
+  public static globFiles(location: string): string[] {
+    return union([], sync(location));
+  }
 }
--- ms-backend/src/config/server.ts
+++ ms-backend/src/config/server.ts
@@ -1,16 +1,49 @@
 import * as bodyParser from "body-parser";
+import * as cookieParser from "cookie-parser";
 import * as express from "express";
+import * as mongoose from "mongoose";
+import * as logger from "morgan";
+import * as path from "path";
 import config from "./config";
 
-const app = express();
+export default function() {
+  const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+  for (const model of config.globFiles(config.models)) {
+    require(path.resolve(model));
+  }
 
-app.listen(config.port, () => {
+  mongoose
+    .connect(config.mongodb, {
+      promiseLibrary: global.Promise,
+      useMongoClient: true,
+    })
+    .catch(() => {
+      console.log("Error connecting to mongo");
+    });
+
+  app.use(logger("dev"));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, "../../src/public")));
+
+  for (const route of config.globFiles(config.routes)) {
+    console.log(route);
+    require(path.resolve(route)).default(app);
+  }
+
+  app.use(
+
+    (req: express.Request, res: express.Response, next: Function): void => {
+      const err: Error = new Error("Not Found");
+      next(err);
+    },
+  );
+
+  app.listen(config.port, () => {
     console.log(`Running on ${config.port}`);
-});
+  });
 
-app.get("/", (req, resp) => {
-    resp.send("inDEX");
-});
+  return app;
+}
