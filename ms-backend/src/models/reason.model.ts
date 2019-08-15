0d66e2f958343504f62a423a25b3d11e40b67d77
Lucas Forain <lucas.forain@ibm.com>
2019-08-15 16:42:16 -0300

    delete lib files

--- ms-backend/src/models/reason.model.ts
+++ ms-backend/src/models/reason.model.ts
@@ -1,5 +1,5 @@
 import * as mongoose from "mongoose";
-import { prop, Typegoose } from "../lib/typegoose";
+import { prop, Typegoose } from "typegoose";
 
 class ReasonType extends Typegoose {
   @prop({ required: true })
