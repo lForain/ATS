44e194fd9b8eb2d98e6ab0c391bdaa34f1c02e94
Bruno Rossato <bruno.araujo@ibm.com>
2019-08-20 11:28:45 -0300

    12-crudReason

--- ms-backend/src/controllers/reason.controller.ts
+++ ms-backend/src/controllers/reason.controller.ts
@@ -4,61 +4,61 @@ import { Reason } from "../models/reason.model";
 export default class ReasonController {
   public async read(req: Request, res: Response): Promise<void> {
     const reasonfilter = req.body && req.body.reasonDescription ? req.body : {};
-    Reason.find(reasonfilter, (err, reasons) => {
-      try {
+    try {
+      Reason.find(reasonfilter, (err, reason) => {
         res.send({
-          data: reasons,
-          message: "Reason(s) sucessfully returned.",
+          data: reason,
+          message: "Reason(s) were successfully returned",
           status: true,
         });
-      } catch (err) {
-        res.send({
-          message: err.message,
-          status: false,
-        });
-      }
-    });
+      });
+    } catch (err) {
+      res.send({
+        message: err.message,
+        status: false,
+      });
+    }
   }
 
   public async create(req: Request, res: Response): Promise<void> {
     try {
       const reason = req.body;
-      await Reason.create(reason);
+      await Reason.create(reason)
       res.send({ status: true });
     } catch (err) {
       res.send({
-        message: err.message,
+    message: err.message,
         status: false,
       });
-    }
+  }
+}
+
+  public async update(req: Request, res: Response): Promise < void> {
+  try {
+    const query = req.body.reasonDescription;
+    const update = req.body.newReason;
+    await Reason.updateOne({ reasonDescription: query }, { reasonDescription: update });
+    res.send({ status: true });
+  } catch(err) {
+    res.send({
+    message: err.message,
+      status: false,
+      });
+}
   }
 
-  public async update(req: Request, res: Response): Promise<void> {
-    try {
-      const query = req.body.reasonDescription;
-      const update = req.body.newReason;
-      await Reason.updateOne({ reasonDescription: query }, { reasonDescription: update });
-      res.send({ status: true });
-    } catch (err) {
-      res.send({
-        message: err.message,
-        status: false,
-      });
-    }
+  public async delete (req: Request, res: Response): Promise < void> {
+  try {
+    const query = req.body.id;
+    await Reason.deleteOne({ _id: query });
+    res.send({ status: true });
+  } catch(err) {
+    res.send({
+      message: err.message,
+      status: false,
+    });
   }
-
-  public async delete(req: Request, res: Response): Promise<void> {
-    try {
-      const query = req.body.id;
-      await Reason.deleteOne({ _id: query });
-      res.send({ status: true });
-    } catch (err) {
-      res.send({
-        message: err.message,
-        status: false,
-      });
-    }
-  }
+}
 }
 
 export const reasonController = new ReasonController();
