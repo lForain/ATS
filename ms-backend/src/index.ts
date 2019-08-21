6b697e143ab1e623300bde585bfe7e2f24afd6bc
Eduardo Chaves <eduardo.chaves@ibm.com>
2019-08-21 15:43:34 -0300

    add conflict fixes

--- ms-backend/src/index.ts
+++ ms-backend/src/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import * as http from "http";
 import config from "./config/config";
 
@@ -17,8 +16,3 @@ server.on("listening", () => {
     `Server started on port ${config.port} on env ${process.env.NODE_ENV ||
     "dev"} dbcon ${config.mongodb}`);
 });
-=======
-import { Server } from "https";
-
-const app = require("./config/server").default();
->>>>>>> 13a2a2437543525da87ba09a86f6fe18f318f9a7
