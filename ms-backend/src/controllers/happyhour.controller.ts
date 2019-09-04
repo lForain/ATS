c1e295c9401cf03828b296a88cf6dab11bd58225
Lucas Forain <lucas.forain@ibm.com>
2019-09-04 16:27:32 -0300

    commit to pull master

---
+++ ms-backend/src/controllers/happyhour.controller.ts
@@ -0,0 +1,7 @@
+import { Request , Response } from "express";
+import { HHModel } from "../models/happyhour.model";
+
+class HHController {
+
+
+};
---
+++ ms-backend/src/models/happyhour.model.ts
@@ -0,0 +1,25 @@
+import * as mongoose from "mongoose";
+import { prop, Typegoose } from "typegoose";
+
+class HHType extends Typegoose {
+  @prop({ required: true })
+  private local: string;
+
+  @prop({ required: true })
+  private creationDate: Date;
+
+  @prop({ required: true })
+  private eventDate: Date;
+
+  @prop({ required: true })
+  private description: string;
+
+  @prop({ required: true })
+  private usersPresent: string[];
+}
+
+export const HHModel = new HHType().getModelForClass(HHType, {
+  existingConnection: null,
+  existingMongoose: mongoose,
+  schemaOptions: null,
+});
