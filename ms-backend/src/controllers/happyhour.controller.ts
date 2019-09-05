62e29c2a9e8abd37f95dca64ea0af6c88d016c89
Lucas Forain <lucas.forain@ibm.com>
2019-09-05 11:30:54 -0300

    merge from 12-crudReason

--- ms-backend/src/controllers/happyhour.controller.ts
+++ ms-backend/src/controllers/happyhour.controller.ts
@@ -2,6 +2,33 @@ import { Request , Response } from "express";
 import { HHModel } from "../models/happyhour.model";
 
 class HHController {
+  public async read(req: Request, res: Response): Promise<void> {
+    try {
+      const reasonfilter = req.body && req.body.reasonDescription ? req.body : {};
+      const reason = await ReasonModel.find(reasonfilter);
+      res.send({
+        data: reason,
+        message: "Reason(s) were successfully returned",
+        status: true,
+      });
+    } catch (err) {
+      res.send({
+        message: err.message,
+        status: false,
+      });
+    }
+  }
 
-
-};
+  public async create(req: Request, res: Response): Promise<void> {
+    try {
+      const happyhour = req.body;
+      await HHModel.create(happyhour);
+      res.send({ status: true });
+    } catch (err) {
+      res.send({
+        message: err.message,
+        status: false,
+      });
+    }
+  }
+}
