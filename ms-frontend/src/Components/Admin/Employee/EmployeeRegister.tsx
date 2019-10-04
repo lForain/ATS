8d2ce1baa64376d77e41a33dbce02f3c347d068d
Luiz Fernando (luizfcneto <luizfcneto123@gmail.com>
2019-10-04 13:26:18 -0300

    *incrementando métodos para captacao dos inputs de cadastro de funcionarios e produtos

--- ms-frontend/src/Components/Admin/Employee/EmployeeRegister.tsx
+++ ms-frontend/src/Components/Admin/Employee/EmployeeRegister.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import 'materialize-css/dist/js/materialize';
+import axios from 'axios';
+import querystring from 'querystring';
 
+axios.defaults.baseURL = 'localhost:3000';
 
 class EmployeeRegister extends React.Component{
 
@@ -8,7 +11,29 @@ class EmployeeRegister extends React.Component{
         super(props);
     }
 
+    inputListener = ( event: any ) => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value,
+        })
+    }    
 
+    onSubmit = ( event: any ) => {
+        event.preventDefault();
+        console.log( this.state );
+
+        try{
+            const response = axios.post('google.com', querystring.stringify({
+                params: this.state
+            }));
+
+            console.log( response );
+    
+        }catch ( error ){
+            console.log( error );
+        }
+    }
+    
     render(){
 
         return(
@@ -19,14 +44,14 @@ class EmployeeRegister extends React.Component{
                 <div className="row">
 
                     {/* Formulário de Cadastro de Funcionários */}
-                    <form className="col s12">
+                    <form className="col s12" onSubmit={this.onSubmit}>
                         
                         <div className="row">
                             
                             {/* Radio de Tatuador */}
                             <div className="input-field col offset-s1 s2">
                                 <label htmlFor="tatuador">    
-                                    <input className="with-gap" name="tipoFuncionario" type="radio" id="tatuador"/>
+                                    <input className="with-gap" name="tipoFuncionario" type="radio" id="tatuador" onChange={this.inputListener}/>
                                     <span> Tatuador </span>
                                 </label>
                             </div>
@@ -34,7 +59,7 @@ class EmployeeRegister extends React.Component{
                             {/* Radio de Atendente */}
                             <div className="input-field col s2">
                                 <label htmlFor="atendente"> 
-                                    <input className="with-gap" name="tipoFuncionario" type="radio" id="atendente"/>
+                                    <input className="with-gap" name="tipoFuncionario" type="radio" id="atendente" onChange={this.inputListener}/>
                                     <span> Atendente </span>
                                 </label>
                             </div>
@@ -46,25 +71,25 @@ class EmployeeRegister extends React.Component{
                             {/* Campo nome: */}
                             <div className="input-field col offset-s1 s4">
                                 <label htmlFor="name">Nome</label>
-                                <input name="name" ref="name" type="text" className="validate"  />
+                                <input name="name" ref="name" type="text" className="validate"  onChange={this.inputListener}/>
                             </div>
                            
                            {/* Campo Email: */}
                             <div className="input-field col s4">
                                 <label htmlFor="email">E-mail</label>
-                                <input name="email" type="email" className="validate"  />
+                                <input name="email" type="email" className="validate"  onChange={this.inputListener}/>
                             </div>
 
                             {/* Campo Telefone: */}
                             <div className="input-field col offset-s1 s4">
                                 <label htmlFor="telefone">Telefone</label>
-                                <input name="phone" type="number" className="validate" />
+                                <input name="phone" type="number" className="validate" onChange={this.inputListener}/>
                             </div>
 
                             {/* Campo Data de Nascimento: */}
                             <div className="input-field col s4">
                                 <label htmlFor="dtNascimento">Data Nascimento</label>
-                                <input name="birthDate" type="text" className="datepicker" />
+                                <input name="birthDate" type="text" className="datepicker" onChange={this.inputListener} />
                             </div>
 
                         </div>
@@ -73,13 +98,13 @@ class EmployeeRegister extends React.Component{
                             {/* Campo de CPF */}
                             <div className="input-field col offset-s1 s4">
                                 <label htmlFor="cpf">CPF</label>
-                                <input name="cpf" type="number" className="validate" />
+                                <input name="cpf" type="number" className="validate" onChange={this.inputListener} />
                             </div>
 
                             {/* Campo Endereço: */}
                             <div className="input-field col s4">
                                 <label htmlFor="endereco">Endereço</label>
-                                <input name="adress" type="text" className="validate"  />
+                                <input name="adress" type="text" className="validate" onChange={this.inputListener}/>
                             </div>
 
                         </div>
@@ -88,13 +113,13 @@ class EmployeeRegister extends React.Component{
                             {/* Campo Senha: */}
                             <div className="input-field col offset-s1 s4">
                                 <label htmlFor="senha">Senha</label>
-                                <input name="password" type="password" className="validate" />
+                                <input name="password" type="password" className="validate" onChange={this.inputListener} />
                             </div>
                             
                             {/* Campo Confirmar Senha: */}
                             <div className="input-field col s4">
                                 <label htmlFor="confSenha">Confirme sua senha</label>
-                                <input name="confSenha" type="password" className="validate" />
+                                <input name="confSenha" type="password" className="validate" onChange={this.inputListener}/>
                             </div> 
                         </div>
                         
--- ms-frontend/src/Components/Admin/Products/ProductsRegister.tsx
+++ ms-frontend/src/Components/Admin/Products/ProductsRegister.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
+import axios from 'axios';
+import querystring from 'querystring';
+
+axios.defaults.baseURL = 'localhost:3000';
 
 class ProductsRegister extends React.Component{
+
+    inputListener = ( event: any ) => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value,
+        })
+    }    
+
+    onSubmit = ( event: any ) => {
+        event.preventDefault();
+        console.log( this.state );
+
+        try{
+            const response = axios.post('google.com', querystring.stringify({
+                params: this.state
+            }));
+
+            console.log( response );
+    
+        }catch ( error ){
+            console.log( error );
+        }
+    }
 
     render(){
 
@@ -12,14 +39,14 @@ class ProductsRegister extends React.Component{
                 <div className="row">
 
                     {/* Formulário de Cadastro de Produtos */}
-                    <form className="col s12">
+                    <form className="col s12" onSubmit={this.onSubmit}>
                         
                         <div className="row">
                             
                             {/* Radio de Produtos( vendivel ) */}
                             <div className="input-field col offset-s1 s2">
                                 <label htmlFor="produtoVendivel">    
-                                    <input className="with-gap" name="tipoProduto" type="radio" id="produtoVendivel"/>
+                                    <input className="with-gap" name="tipoProduto" ref="tipoProduto" type="radio" id="produtoVendivel" onChange={this.inputListener}/>
                                     <span> Produto para venda </span>
                                 </label>
                             </div>
@@ -27,7 +54,7 @@ class ProductsRegister extends React.Component{
                             {/* Radio de Produtos( consumível ) */}
                             <div className="input-field col s2">
                                 <label htmlFor="produtoConsumivel"> 
-                                    <input className="with-gap" name="tipoProduto" type="radio" id="produtoConsumivel"/>
+                                    <input className="with-gap" name="tipoProduto" ref="tipoProduto" type="radio" id="produtoConsumivel" onChange={this.inputListener}/>
                                     <span> Produto para consumir </span>
                                 </label>
                             </div>
@@ -39,25 +66,25 @@ class ProductsRegister extends React.Component{
                             {/* Campo nome: */}
                             <div className="input-field col offset-s1 s4">
                                 <label htmlFor="name">Nome</label>
-                                <input name="name" ref="name" type="text" className="validate"  />
+                                <input name="name" ref="name" type="text" className="validate"  onChange={this.inputListener}/>
                             </div>
                            
                            {/* Campo Descrição: */}
                             <div className="input-field col s4">
                                 <label htmlFor="descricao">Descrição</label>
-                                <input name="descricao" type="text" className="validate"  />
+                                <input name="descricao" type="text" className="validate"  onChange={this.inputListener}/>
                             </div>
 
                             {/* Campo Telefone do Fornecedor: */}
                             <div className="input-field col offset-s1 s4">
                                 <label htmlFor="telefone">Telefone do Fornecedor</label>
-                                <input name="phone" type="number" className="validate" />
+                                <input name="phone" type="number" className="validate" onChange={this.inputListener}/>
                             </div>
 
                             {/* Campo Email do Fornecedor: */}
                             <div className="input-field col s4">
                                 <label htmlFor="emailFornecedor">Email do fornecedor</label>
-                                <input name="emailFornecedor" type="email" className="validate" />
+                                <input name="emailFornecedor" type="email" className="validate" onChange={this.inputListener}/>
                             </div>
 
                         </div>
@@ -66,13 +93,13 @@ class ProductsRegister extends React.Component{
                             {/* Campo de Preço de compra do Fornecedor: */}
                             <div className="input-field col offset-s1 s4">
                                 <label htmlFor="precoCompra">Preço Compra</label>
-                                <input name="precoCompra" type="number" className="validate" step="0.01" min="0" />
+                                <input name="precoCompra" type="number" className="validate" step="0.01" min="0" onChange={this.inputListener}/>
                             </div>
 
                             {/* Campo de Preço de venda do fornecedor */}
                             <div className="input-field col s4">
                                 <label htmlFor="precoVenda">Preço Venda</label>
-                                <input name="precoVenda" type="number" className="validate" step="1" min="0" />
+                                <input name="precoVenda" type="number" className="validate" step="1" min="0" onChange={this.inputListener}/>
                             </div>
 
                         </div>
@@ -82,14 +109,14 @@ class ProductsRegister extends React.Component{
                             {/* Campo nome do Fornecedor */}
                             <div className="input-field col offset-s1 s4">
                                 <label htmlFor="nomeFornecedor"> Nome do Fornecedor </label>
-                                <input name="nomeFornecedor" type="text" className="validate"/>
+                                <input name="nomeFornecedor" type="text" className="validate" onChange={this.inputListener}/>
 
                             </div>
 
                             {/* Campo de Quantidade de Produtos */}
                             <div className="input-field col s4">
                                 <label htmlFor="quantidade">Quantidade</label>
-                                <input name="quantidade" type="number" className="validate" step="1" min="0" />
+                                <input name="quantidade" type="number" className="validate" step="1" min="0" onChange={this.inputListener}/>
                             </div>
 
                         </div>
@@ -103,7 +130,7 @@ class ProductsRegister extends React.Component{
                                 </div>
 
                                 <div className="file-path-wrapper">
-                                    <input className="file-path validate" type="text" placeholder="Upload one or more files"/>
+                                    <input className="file-path validate" type="text" placeholder="Upload one or more files" onChange={this.inputListener}/>
                                 </div>
                             </div>
                             
