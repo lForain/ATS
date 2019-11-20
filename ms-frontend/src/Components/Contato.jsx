d802c3d176ea4c1880cd4955ce5d2118666fb8c7
Luiz Fernando (luizfcneto <luizfcneto123@gmail.com>
2019-11-20 20:46:13 -0300

    *Pagina de contato refatorada

--- ms-frontend/src/Components/Contato.jsx
+++ ms-frontend/src/Components/Contato.jsx
@@ -26,36 +26,62 @@ class Contato extends Component {
 
     render() {
         return (
-            <div>
-                <div className="row col offset-s1"> <h2> Contato </h2> </div>
-                <div className="row col offset-s3 col s6">
-                    <p class="flow-text">Olá, aqui no estúdio do Seu Armando prezamos pela sua satisfação.
+            <div className="row">
+              <div className="col offset-s1"> <h2> Contato </h2> </div>
+                
+              <div className="row"> 
+                <div className="col offset-s3 col s6">
+                  <p className="flow-text">Olá, aqui no estúdio do Seu Armando prezamos pela sua satisfação.
                     Por isso estamos sempre ansiosos para receber seu feedback ou responder qualquer dúvida que você possa ter.
                     Portanto sinta-se a vontade a nos contatar por qualquer um dos métodos abaixo:</p>
                 </div>
-                <h5>Endereço</h5>
-                <p>Rua Exemplo, número 0</p>
-                <h5>Instagram</h5>
-                <p>instagram.com/estudioSeuArmando</p>
-                <h5>Whatsapp:</h5>
-                <p>(21) 99999-9999</p>
-                <h5>Email:</h5>
-                <p>estudioseuarmando@gmail.com</p>
+              </div>
 
-                <div class="row">
-                    <form class="col s12">
-                        <div class="row">
-                            <p class="flow-text">Ou nos envie um email diretamente por aqui:</p>
-                            <div class="input-field col s12">
-                            <textarea id="textarea1" name="corpoEmail" class="materialize-textarea onChange={this.inputListener}"/>
-                            <label for="textarea1">Textarea</label>
-                            </div>
-                        </div>
-                        <div className="row col offset-s1">
-                            <button type="submit" value="submit form" className="waves-effect waves-light btn black">Enviar</button>
-                        </div>
-                    </form>
+              <div className="row">
+                <div className="col offset-s1">
+                  <h5>Endereço:</h5>
+                  <p>Rua Exemplo, número 0</p>
                 </div>
+                  
+                <div className="col offset-s1">
+                  <h5>Instagram:</h5>
+                  <p>instagram.com/estudioSeuArmando</p>
+                </div>
+                 
+                <div className="col offset-s1">
+                  <h5>Whatsapp:</h5>
+                  <p>(21) 99999-9999</p>
+                </div>
+
+                <div className="col offset-s1">
+                  <h5>Email:</h5>
+                  <p>estudioseuarmando@gmail.com</p>
+                </div>
+
+              </div>
+
+              <div className="row">
+                <form className="col s8 offset-s2">
+                  <p className="flow-text"> Ou entre em contato conosco por email mandando a mensagem abaixo: </p>
+                  
+                  {/* Campo do corpo do Email */}
+                  <div className="input-field">
+                    <textarea id="textarea1" name="corpoEmail" className="materialize-textarea" onChange={ this.inputListener }/>
+                    <label htmlFor="textarea1"> Texto do E-mail: </label>
+                  </div>
+
+                  {/* Botão de Enviar */}
+                  <div className="row">
+                    <div className="col offset-s1">
+                      <button type="submit" value="submit form" className="waves-effect waves-light btn black"> Enviar </button>
+
+                    </div>
+                  </div>
+
+                </form>
+
+              </div>
+
             </div>
         )
     }
